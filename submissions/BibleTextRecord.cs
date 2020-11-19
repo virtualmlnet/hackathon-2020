1969083f61a1248f6b4a8ad3aa0b5c9107242aea
asacin <antonio.sacin@gmail.com>
2020-11-19 01:36:01 -0600

    Add files via upload
    Creating a new branch for the submission of Team 12 (Charax) - Text Analytics for Education.

---
+++ submissions/BibleTextRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace myMLApp.DataModels
+{
+    public class BibleTextRecord
+    {
+        public int VerseID { get; set; }
+
+        public int BookID { get; set; }
+
+        public int Chapter { get; set; }
+
+        public int Versicle { get; set; }
+
+        public string Text { get; set; }
+    }
+}
---
+++ submissions/BibleVersionInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace myMLApp.DataModels
+{
+    /// <summary>
+    /// This class contains the records for the bible version in abbreviated and full text.
+    /// </summary>
+    public class BibleVersionInfo
+    {
+        public int VersionID { get; set; }
+
+        public string VersionFileName { get; set; }
+
+        public string VersionAbbreviation { get; set; }
+
+        public string VersionLanguage { get; set; }
+
+        public string VersionName { get; set; }
+
+        public string VersionDocumentation { get; set; }
+
+        public string VersionDomain { get; set; }
+    }
+
+    internal enum BibleVersion
+    {
+        ASV,   // American Standard-ASV1901
+        BBE,   // Bible in Basic English
+        DARBY, // Darby English Bible
+        KJV,   // King James Version
+        WBT,   // Webster's Bible
+        WEB,   // World English Bible
+        YLT    // Young's Literal Translation
+    }
+}
